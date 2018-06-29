3fba0e61dede99e597e917d520e3339b18de05c8
Mitchell Feinstein <mitch.feinstein@mlb.com>
2018-06-29 15:24:16 -0400

    TIX-807 sharingProhibited: See https://github.com/ryantenney/passkit4j/pull/46

--- src/main/java/com/ryantenney/passkit4j/Pass.java
+++ src/main/java/com/ryantenney/passkit4j/Pass.java
@@ -26,174 +26,185 @@ import com.ryantenney.passkit4j.model.PassInformation;
 
 @Data
 @NoArgsConstructor
-@Accessors(chain=true, fluent=true)
+@Accessors(chain = true, fluent = true)
 public class Pass {
 
 
-	// Standard Keys
+    // Standard Keys
 
-	@NonNull private String description;
-	@NonNull private String organizationName;
-	@NonNull private String passTypeIdentifier;
-	@NonNull private String serialNumber;
-	@NonNull private String teamIdentifier;
-
-	@JsonProperty("formatVersion") private final int $formatVersion = 1;
-
-
-	// Visual Appearance Keys
-
-	private Barcode barcode;
-	private List<Barcode> barcodes;
-	
-	private Color backgroundColor;
-	private Color foregroundColor;
-	private String groupingIdentifier;
-	private Color labelColor;
-	private String logoText;
-	@JsonInclude(Include.NON_DEFAULT) private boolean suppressStripShine = false;
+    @JsonProperty("formatVersion")
+    private final int $formatVersion = 1;
+    @NonNull
+    private String description;
+    @NonNull
+    private String organizationName;
+    @NonNull
+    private String passTypeIdentifier;
+    @NonNull
+    private String serialNumber;
+    @NonNull
+    private String teamIdentifier;
 
 
-	// Web Service Keys
+    // Visual Appearance Keys
+    private Barcode barcode;
+    private List<Barcode> barcodes;
 
-	private String webServiceURL;
-	private String authenticationToken;
-
-
-	// Relevance Keys
-	private boolean ignoresTimeZone = false;
-	private List<Location> locations;
-	private List<Beacon> beacons;
-	private Integer maxDistance;
-	private Date relevantDate;
-
-
-	// Associated App Keys
-
-	private List<Long> associatedStoreIdentifiers;
-	private String appLaunchURL;
+    private Color backgroundColor;
+    private Color foregroundColor;
+    private String groupingIdentifier;
+    private Color labelColor;
+    private String logoText;
+    @JsonInclude(Include.NON_DEFAULT)
+    private boolean suppressStripShine = false;
 
 
-	// Companion App Keys
+    // Web Service Keys
 
-	private JsonNode userInfo;
-
-
-	// Expiration Keys
-
-	private Date expirationDate;
-	@JsonInclude(Include.NON_DEFAULT) private boolean voided = false;
+    private String webServiceURL;
+    private String authenticationToken;
 
 
-	// NFC Keys
-	private NFC nfc;
+    // Relevance Keys
+    private boolean ignoresTimeZone = false;
+    private List<Location> locations;
+    private List<Beacon> beacons;
+    private Integer maxDistance;
+    private Date relevantDate;
 
 
-	@JsonIgnore private PassInformation passInformation;
-	@JsonIgnore private List<NamedInputStreamSupplier> files;
+    // Associated App Keys
+
+    private List<Long> associatedStoreIdentifiers;
+    private String appLaunchURL;
 
 
-	public List<Barcode> barcodes() {
-		return this.barcodes;
-	}
-	
-	public Pass barcodes(List<Barcode> values) {
-		this.barcodes = values;
-		return this;
-	}
-	
-	public Pass barcodes(Barcode... values) {
-		this.barcodes = Arrays.asList(values);
-		return this;
-	}
-	
-	public List<NamedInputStreamSupplier> files() {
-		if (this.files == null) {
-			return Collections.emptyList();
-		}
-		return this.files;
-	}
+    // Companion App Keys
 
-	public Pass files(List<NamedInputStreamSupplier> values) {
-		this.files = values;
-		return this;
-	}
+    private JsonNode userInfo;
 
-	public Pass files(NamedInputStreamSupplier... values) {
-		this.files = Arrays.asList(values);
-		return this;
-	}
 
-	public List<Location> locations() {
-		return this.locations;
-	}
+    // Expiration Keys
 
-	public Pass locations(List<Location> values) {
-		this.locations = values;
-		return this;
-	}
+    private Date expirationDate;
+    @JsonInclude(Include.NON_DEFAULT)
+    private boolean voided = false;
 
-	public Pass locations(Location... values) {
-		this.locations = Arrays.asList(values);
-		return this;
-	}
 
-	public List<Beacon> beacons() {
-		return this.beacons;
-	}
+    // NFC Keys
+    private NFC nfc;
 
-	public Pass beacons(List<Beacon> values) {
-		this.beacons = values;
-		return this;
-	}
+    // Extended properties
+    @JsonInclude(Include.NON_DEFAULT)
+    private boolean sharingProhibited = false;
 
-	public Pass beacons(Beacon... values) {
-		this.beacons = Arrays.asList(values);
-		return this;
-	}
+    @JsonIgnore
+    private PassInformation passInformation;
+    @JsonIgnore
+    private List<NamedInputStreamSupplier> files;
 
-	public List<Long> associatedStoreIdentifiers() {
-		return this.associatedStoreIdentifiers;
-	}
 
-	public Pass associatedStoreIdentifiers(List<Long> values) {
-		this.associatedStoreIdentifiers = values;
-		return this;
-	}
+    public List<Barcode> barcodes() {
+        return this.barcodes;
+    }
 
-	public Pass associatedStoreIdentifiers(Long... values) {
-		this.associatedStoreIdentifiers = Arrays.asList(values);
-		return this;
-	}
+    public Pass barcodes(List<Barcode> values) {
+        this.barcodes = values;
+        return this;
+    }
 
-	public Pass relevantDate(Date relevantDate) {
-		this.relevantDate = relevantDate;
-		return this;
-	}
+    public Pass barcodes(Barcode... values) {
+        this.barcodes = Arrays.asList(values);
+        return this;
+    }
 
-	public Pass relevantDate(long timestamp) {
-		this.relevantDate = new Date(timestamp);
-		return this;
-	}
+    public List<NamedInputStreamSupplier> files() {
+        if (this.files == null) {
+            return Collections.emptyList();
+        }
+        return this.files;
+    }
 
-	public Pass relevantDate(String iso8601dateString) {
-		this.relevantDate = ISO8601Utils.parse(iso8601dateString);
-		return this;
-	}
+    public Pass files(List<NamedInputStreamSupplier> values) {
+        this.files = values;
+        return this;
+    }
 
-	public Pass expirationDate(Date expirationDate) {
-		this.expirationDate = expirationDate;
-		return this;
-	}
+    public Pass files(NamedInputStreamSupplier... values) {
+        this.files = Arrays.asList(values);
+        return this;
+    }
 
-	public Pass expirationDate(long timestamp) {
-		this.expirationDate = new Date(timestamp);
-		return this;
-	}
+    public List<Location> locations() {
+        return this.locations;
+    }
 
-	public Pass expirationDate(String iso8601dateString) {
-		this.expirationDate = ISO8601Utils.parse(iso8601dateString);
-		return this;
-	}
+    public Pass locations(List<Location> values) {
+        this.locations = values;
+        return this;
+    }
+
+    public Pass locations(Location... values) {
+        this.locations = Arrays.asList(values);
+        return this;
+    }
+
+    public List<Beacon> beacons() {
+        return this.beacons;
+    }
+
+    public Pass beacons(List<Beacon> values) {
+        this.beacons = values;
+        return this;
+    }
+
+    public Pass beacons(Beacon... values) {
+        this.beacons = Arrays.asList(values);
+        return this;
+    }
+
+    public List<Long> associatedStoreIdentifiers() {
+        return this.associatedStoreIdentifiers;
+    }
+
+    public Pass associatedStoreIdentifiers(List<Long> values) {
+        this.associatedStoreIdentifiers = values;
+        return this;
+    }
+
+    public Pass associatedStoreIdentifiers(Long... values) {
+        this.associatedStoreIdentifiers = Arrays.asList(values);
+        return this;
+    }
+
+    public Pass relevantDate(Date relevantDate) {
+        this.relevantDate = relevantDate;
+        return this;
+    }
+
+    public Pass relevantDate(long timestamp) {
+        this.relevantDate = new Date(timestamp);
+        return this;
+    }
+
+    public Pass relevantDate(String iso8601dateString) {
+        this.relevantDate = ISO8601Utils.parse(iso8601dateString);
+        return this;
+    }
+
+    public Pass expirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+        return this;
+    }
+
+    public Pass expirationDate(long timestamp) {
+        this.expirationDate = new Date(timestamp);
+        return this;
+    }
+
+    public Pass expirationDate(String iso8601dateString) {
+        this.expirationDate = ISO8601Utils.parse(iso8601dateString);
+        return this;
+    }
 
 }
